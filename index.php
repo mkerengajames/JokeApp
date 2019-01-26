582252a234d8b301fb57324cb000a7b77e5914b6
james mkerenga <mkerengajames@gmail.com>
2019-01-26 04:29:30 +0300

    created index

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <title>
+        JokeApp | The Laughing world
+    </title>
+    <script language="javascript" type="text/javascript">
+        alert("hello");
+    </script>
+</head>
+<body>
+    <table>
+        <tr>
+            <td>
+                1
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mkere
+ * Date: 1/26/2019
+ * Time: 4:25 AM
+ */
